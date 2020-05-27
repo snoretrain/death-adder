e476f65ff2cac0728e0aaebd9d45377af991a058
David Sornberger <david@helium.com>
2020-05-26 22:29:18 -0700

    Updates to export Middleware from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Server, Request, Response } from './network';
 export { default as Router } from './router';
 export { default as Endpoint } from './Endpoint';
+export { Middleware } from './middleware';
